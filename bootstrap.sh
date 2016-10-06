0e93507bb0ca1cd2a90b94d791815ac3cbc32fc6
matt <matt@matt-jenkins>
2016-10-06 14:07:26 -0700

    Add various files.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,4 +5,19 @@
 #   * eth0 is DHCP/default route
 #   * eth1 is host-only network
 #
-# It will be configured with DHCP on eth1 and to connect to KTI's Salt master. 
+
+# Configure eth1
+wget https://raw.githubusercontent.com/kti-matt/host-bootstrap/master/authorized_keys -O /etc/network/interfaces.d/eth1
+ifup eth1
+
+# Configure sudo
+apt-get install -y sudo
+adduser matt sudo
+mkdir --parent /root/.ssh
+wget https://raw.githubusercontent.com/kti-matt/host-bootstrap/master/authorized_keys -O /root/.ssh/authorized_keys
+mkdir --parent /home/matt/.ssh
+cp /root/.ssh/authorized_keys /home/matt/.ssh/authorized_keys
+chmod -R matt:matt /home/matt/.ssh
+
+# Configure salt
+
