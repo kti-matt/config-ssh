908b7e64f0b2f423c98954b1a0c51465e3ccf971
Matt Fox <matt@kerkhofftech.ca>
2016-12-08 18:30:12 -0800

    Add conditions to bootstrap.sh

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ To bootstrap a VirtualBox host, do:
 
     wget https://git.io/vPWEz -O - | bash
 
+You can omit some or all steps with:
+
+    ETH=false SALT=false SUDO=false wget https://git.io/vPWEz -O - | bash
+
 To just add my public key to authorized_keys, do:
 
     wget https://git.io/vXzYD -O .ssh/authorized_keys
--- bootstrap.sh
+++ bootstrap.sh
@@ -5,24 +5,36 @@
 #   * eth0 is DHCP/default route
 #   * eth1 is host-only network
 
+# Use these environment variables to allow configurations to be skipped.
+ETH=${CONF_ETH:-true}
+SALT=${CONF_SALT:-true}
+SUDO=${CONF_SUDO:-true}
+USER=${SUDO_USER:-matt}
+
 # Configure eth1
-wget https://raw.githubusercontent.com/kti-matt/host-bootstrap/master/eth1 -O /etc/network/interfaces.d/eth1
-ifup eth1
+if [ "$ETH" = "true" ]; then
+  wget https://raw.githubusercontent.com/kti-matt/host-bootstrap/master/eth1 -O /etc/network/interfaces.d/eth1
+  ifup eth1
+fi
 
 # Configure salt
-wget https://raw.githubusercontent.com/kti-matt/host-bootstrap/master/SaltStack-Packaging-Team-DE57BFBE.pub -O - -q | apt-key add -
-wget https://raw.githubusercontent.com/kti-matt/host-bootstrap/master/saltstack.list -O /etc/apt/sources.list.d/saltstack.list
-apt-get update
-apt-get install -y salt-minion
-wget https://raw.githubusercontent.com/kti-matt/host-bootstrap/master/minion -O /etc/salt/minion
-wget https://raw.githubusercontent.com/kti-matt/host-bootstrap/master/grains -O /etc/salt/grains
-systemctl restart salt-minion
+if [ "$SALT" = "true" ]; then
+  wget https://raw.githubusercontent.com/kti-matt/host-bootstrap/master/SaltStack-Packaging-Team-DE57BFBE.pub -O - -q | apt-key add -
+  wget https://raw.githubusercontent.com/kti-matt/host-bootstrap/master/saltstack.list -O /etc/apt/sources.list.d/saltstack.list
+  apt-get update
+  apt-get install -y salt-minion
+  wget https://raw.githubusercontent.com/kti-matt/host-bootstrap/master/minion -O /etc/salt/minion
+  wget https://raw.githubusercontent.com/kti-matt/host-bootstrap/master/grains -O /etc/salt/grains
+  systemctl restart salt-minion
+fi
 
 # Configure sudo
-apt-get install -y sudo
-adduser matt sudo
-mkdir --parent /root/.ssh
-wget https://raw.githubusercontent.com/kti-matt/host-bootstrap/master/authorized_keys -O /root/.ssh/authorized_keys
-mkdir --parent /home/matt/.ssh
-cp /root/.ssh/authorized_keys /home/matt/.ssh/authorized_keys
-chown -R matt:matt /home/matt/.ssh
+if [ "$SUDO" = "true" ]; then
+  apt-get install -y sudo
+  adduser ${USER} sudo
+  mkdir --parent /root/.ssh
+  wget https://raw.githubusercontent.com/kti-matt/host-bootstrap/master/authorized_keys -O /root/.ssh/authorized_keys
+  mkdir --parent /home/${USER}/.ssh
+  cp /root/.ssh/authorized_keys /home/${USER}/.ssh/authorized_keys
+  chown -R ${USER}:${USER} /home/${USER}/.ssh
+fi
